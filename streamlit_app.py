5d1b18d1f9e26808416bcd0029617d7738925845
asiankevinhart <kevinshiyuwei2@gmail.com>
2025-08-09 12:18:05 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 import pandas as pd
 
-df = pd.read_cvs('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
 df
